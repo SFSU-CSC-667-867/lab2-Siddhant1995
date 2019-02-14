cbed502467b4d680958872b5245cb9deaeb3749f
Siddhant Padve <siddhantpadve@sfs-wifi-dhcp-10-143-238-145.wifi.sfsu.edu>
2019-02-13 20:14:35 -0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hi, this is Siddhant.'))
+app.get('/hello', (req, res) => res.send('world.'));
+app.get('/add', (req, res) => res.send('0'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
